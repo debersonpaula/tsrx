4b73bfba112c582b841fbe7c72808e5faad6adf9
Deberson de Paula <deberson.paula@gmail.com>
2021-07-07 19:24:42 -0300

    updated utils

--- src/config/utils/babelPresets.ts
+++ src/config/utils/babelPresets.ts
@@ -15,32 +15,8 @@ export const babelPresets = (env: EnvType) => {
         },
       },
     ],
-    (isEnvProduction || isEnvDevelopment) && [
-      // Latest stable ECMAScript features
-      '@babel/preset-env',
-      {
-        // Allow importing core-js in entrypoint and use browserlist to select polyfills
-        useBuiltIns: 'entry',
-        // Set the corejs version we are using to avoid warnings in console
-        // This will need to change once we upgrade to corejs@3
-        corejs: 3,
-        // Do not transform modules to CJS
-        modules: false,
-        // Exclude transforms that make all code slower
-        exclude: ['transform-typeof-symbol'],
-      },
-    ],
-    [
-      '@babel/preset-react',
-      {
-        // Adds component stack to warning messages
-        // Adds __self attribute to JSX which React will use for some warnings
-        development: isEnvDevelopment || isEnvTest,
-        // Will use the native built-in instead of trying to polyfill
-        // behavior for any plugins that require one.
-        useBuiltIns: true,
-      },
-    ],
+    (isEnvProduction || isEnvDevelopment) && '@babel/env',
+    '@babel/react',
     '@babel/preset-typescript',
   ].filter(Boolean);
 };
