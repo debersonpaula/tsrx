255de9541918064c3eaf5218b026a0b587fc0f08
Deberson de Paula <deberson.paula@gmail.com>
2021-08-05 20:00:33 -0300

    fixed decorator reflection

--- src/config/utils/babelPlugins.ts
+++ src/config/utils/babelPlugins.ts
@@ -4,6 +4,7 @@ export const babelPlugins = (env: EnvType) => {
   const isEnvDevelopment = env === 'development';
 
   return [
+    'babel-plugin-transform-typescript-metadata',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     '@babel/plugin-transform-runtime',
